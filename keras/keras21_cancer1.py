24748a9f864d500ad048e7138e5740a02d9881fe
study <sswwd95@gmail.com>
2021-01-04 14:21:54 +0900

    01.04

--- keras/keras21_cancer1.py
+++ keras/keras21_cancer1.py
@@ -12,8 +12,18 @@ x = datasets.data
 y = datasets.target
 print(x.shape) # (569, 30)
 print(y.shape)  # (569,) 
-print(x[:5])
-print(y)
+# print(x[:5])
+# print(y)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size = 0.8, random_state = 66)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
 # 전처리 알아서 해 / minmax, train_test_split
 
@@ -22,7 +32,10 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 model = Sequential()
-model.add(Dense(1, activation='relu', input_shape=(30,)))
+model.add(Dense(10, activation='relu', input_shape=(30,)))
+model.add(Dense(10, activation='sigmoid'))
+model.add(Dense(10, activation='sigmoid'))
+model.add(Dense(10, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 # 히든레이어 없어도 괜찮다. 
 
@@ -30,5 +43,21 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 model.fit(x,y, epochs=100, validation_split=0.2)
 
-loss = model.evaluate(x,y)
-print(loss)
+#4. 평가, 예측
+loss, acc = model.evaluate(x_test,y_test, batch_size=8)
+print('loss, acc : ', loss, acc)
+
+y_predict = model.predict(x_test)
+print(y_predict)
+
+y_predict = model.predict(x[-5:-1])
+print(y[-5:-1])
+
+'''
+# 실습1. acc 0.985
+#실습2 .predict 출력
+y[-5:-1] =? 0 or 1
+y_pred = model.predict(x[-5:-1])
+print(y_pred)
+print(y[-5: -1]) #끝에서 부터 5개
+'''
