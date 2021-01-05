01def9dd4c99766d26f46c932cf84e093e7a25ad
study <sswwd95@gmail.com>
2021-01-05 19:18:24 +0900

    01.05

---
+++ practice/keras copy/keras22_1_iris2_sklearn.py
@@ -0,0 +1,84 @@
+import numpy as np
+from sklearn.datasets import load_iris
+
+#1. 데이터
+
+# x,y = load_iris(return_X_y=True) 아래와 같은 방법인데 아래 방법이 더 좋다. 
+dataset = load_iris()
+x = dataset.data
+y = dataset.target
+# print(dataset.DESCR)
+# print(dataset.feature_names)
+print(x.shape) # (150,4)
+print(y.shape) # (150,)
+print(x[:5])
+print(y)
+# 꽃이 3 종류(y값이 3개)
+# 0=1 0 0, 1=0 1 0, 2 = 0 0 1
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size = 0.8, random_state = 66)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,
+                                                  test_size=0.4, shuffle=True)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+x_val = scaler.transform(x_val)
+
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+y_val = y_val.reshape(-1,1)
+
+## 원핫인코딩 OneHotEncoding
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+enc.fit(y_train)
+y_train = enc.transform(y_train).toarray()
+y_test = enc.transform(y_test).toarray()
+y_val = enc.transform(y_val).toarray()
+
+print(y)
+print(x.shape) #(150,4)
+print(y.shape) # (150,3) -> reshape됨
+
+# 2. 모델구성
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape=(4,)))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+# 3. 컴파일
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs= 100, batch_size=8)
+
+#3. 평가, 예측
+
+loss,acc = model.evaluate(x_test, y_test, batch_size=8)
+print("loss, acc : ", loss, acc)
+
+y_predict = model.predict(x_test[-5:-1])
+print(y_predict)
+print(y_test[-5:-1])
+print(np.argmax(y_predict,axis=-1))
+#결과치 나오게 코딩할것.   #argmax
+
+# loss, acc :  0.09609115868806839 0.9666666388511658
+# [[1.61500391e-09 3.95920433e-05 9.99960423e-01]
+#  [9.99981046e-01 1.89530183e-05 1.12919353e-12]
+#  [9.84727979e-01 1.52718639e-02 1.03585734e-07]
+#  [7.72912681e-05 4.75085050e-01 5.24837613e-01]]
+# [[0. 0. 1.]
+#  [1. 0. 0.]
+#  [1. 0. 0.]
+#  [0. 0. 1.]]
+# [2 0 0 2]
