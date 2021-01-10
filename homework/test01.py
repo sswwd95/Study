ac74b0139d76d4e51077fcb042957ab5dd3c9ad4
sswwd95 <sswwd95@gmail.com>
2021-01-10 15:30:26 +0900

    01.10

--- homework/test01.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 2
-c = a+b
-print(c)
-
-import tensorflow as tf
-import keras
-
-print("잘 설치했다 ")
--- keras/keras35_2_load_model.py
+++
@@ -1,4 +0,0 @@
-from tensorflow.keras.models import load_model
-model = load_model('./model/save_keras35.h5')
-
-model.summary()
