005a6d691a1d84b29356c3b443b56d78a3be07f6
study <sswwd95@gmail.com>
2021-02-17 15:36:18 +0900

    commit

---
+++ ml/m42_xgb_gpu.py
@@ -0,0 +1,29 @@
+from xgboost import XGBClassifier,XGBRegressor
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import r2_score
+
+datasets = load_boston()
+x = datasets.data
+y = datasets['target']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.8, random_state=66
+)
+
+model = XGBRegressor(n_estimators=100000, learning_rate=0.01,
+                    tree_method='gpu_hist',
+                    predictor='cpu_predictor',
+                    # predictor='gpu_predictor'
+                    gpu_id=0 # gpu 선택할 수 있다.
+)
+model.fit(x_train, y_train, verbose=1, eval_metric=['rmse'],
+        eval_set=[(x_train, y_train),(x_test,y_test)],
+        early_stopping_rounds=10000)
+
+aaa = model.score(x_test, y_test)
+print('model.score : ', aaa)
+
+# 공식문서에서 xgbosst gpu 찾아보기
+# cpu, gpu 분산해서 돌릴 수 있다.
