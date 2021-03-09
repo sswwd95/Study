66d07be69ec156a6acdedd9e80373f3f7cd9e799
study <sswwd95@gmail.com>
2021-03-09 12:01:53 +0900

    commit

---
+++ keras2/keras86_wine_quality.py
@@ -0,0 +1,80 @@
+
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier #훈련 과정에서 구성한 다수의 결정 트리들을 랜덤하게 학습시켜 분류 또는 회귀의 결과도출에 사용
+
+
+wine_data = pd.read_csv('../data/csv/winequality-white.csv',sep=';',dtype=float)
+
+x_data = wine_data.iloc[:,:-1]
+y_data = wine_data.iloc[:,-1]
+
+# Score 값이 7보다 작으면 0,  7보다 크거나 같으면 1로 값 변경.
+y_data = np.array([1 if i>=7 else 0 for i in y_data])
+
+# score 값을 6 기준. 
+# y_data = np.array([1 if i>=7 elif i=6 0 for i in y_data])
+
+# 트레인, 테스트 데이터 나누기.
+train_x, test_x, train_y, test_y = train_test_split(
+    x_data, y_data, test_size = 0.3,random_state=42)
+
+from sklearn.pipeline import Pipeline, make_pipeline 
+from xgboost import XGBClassifier, plot_importance
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, PowerTransformer, RobustScaler, MaxAbsScaler
+
+
+parameters = [
+    {'a__n_estimators': [100,200,300], 'a__learning_late' : [0.1,0.01,0.001],
+    'a__max_depth' : [6,8,10], 'a__colsample_bytree' : [0.6,0.8,1]},
+    {'a__n_estimators': [100,200,300], 'a__learning_late' : [0.1,0.01,0.001],
+    'a__max_depth' : [6,8,10], 'a__colsample_bylevel' : [0.6,0.8,1]}
+]
+
+pipe = Pipeline([('scaler', PowerTransformer()),('a', XGBClassifier(n_jobs=-1))])
+
+# 2. 모델구성
+model = RandomizedSearchCV(pipe, parameters, cv=5)
+
+# model = RandomForestClassifier()
+model.fit(train_x, train_y)
+
+acc = model.score(test_x, test_y)
+print('acc : ', acc)
+
+y_pred = model.predict(test_x)
+
+from sklearn.metrics import classification_report
+
+y_true, y_pred = test_y, model.predict(test_x)
+
+print(classification_report(y_true, y_pred))
+
+'''
+model = RandomForestClassifier()
+acc :  0.8836734693877552
+              precision    recall  f1-score   support
+
+           0       0.89      0.97      0.93      1141
+           1       0.84      0.60      0.70       329
+
+    accuracy                           0.88      1470
+   macro avg       0.87      0.78      0.81      1470
+weighted avg       0.88      0.88      0.88      1470
+'''
+
+'''
+model = RandomizedSearchCV(pipe, parameters, cv=5)
+acc :  0.8836734693877552
+              precision    recall  f1-score   support
+
+           0       0.90      0.95      0.93      1141
+           1       0.80      0.64      0.71       329
+
+    accuracy                           0.88      1470
+   macro avg       0.85      0.80      0.82      1470
+weighted avg       0.88      0.88      0.88      1470
+'''
---
+++ ml/m48_wine_quality1.py
@@ -0,0 +1,73 @@
+# k86_wine_quality 파일에서 실습 후 수업!
+
+from sklearn.datasets import load_wine
+import numpy as np
+import pandas as pd
+
+############기존 wine ##########
+datasets = load_wine()
+x = datasets.data
+y = datasets.target
+
+print(x.shape, y.shape)
+# (178, 13) (178,)
+print(np.unique(y))
+# [0 1 2]
+####### 의미없음 ^^ ############
+
+
+wine = pd.read_csv('../data/csv/winequality-white.csv', sep=';', index_col=None, header=0)
+
+print(wine)
+'''
+      fixed acidity  volatile acidity  citric acid  residual sugar  chlorides  free sulfur dioxide  total sulfur dioxide  density    pH  sulphates  alcohol  quality
+0               7.0              0.27         0.36            20.7      0.045                 45.0                 170.0  1.00100  3.00       0.45      8.8        6
+1               6.3              0.30         0.34             1.6      0.049                 14.0                 132.0  0.99400  3.30       0.49      9.5        6
+2               8.1              0.28         0.40             6.9      0.050                 30.0                  97.0  0.99510  3.26       0.44     10.1        6
+3               7.2              0.23         0.32             8.5      0.058                 47.0                 186.0  0.99560  3.19       0.40      9.9        6
+4               7.2              0.23         0.32             8.5      0.058                 47.0                 186.0  0.99560  3.19       0.40      9.9        6
+...             ...               ...          ...             ...        ...                  ...                   ...      ...   ...        ...      ...      ...
+4893            6.2              0.21         0.29             1.6      0.039                 24.0                  92.0  0.99114  3.27       0.50     11.2        6
+4894            6.6              0.32         0.36             8.0      0.047                 57.0                 168.0  0.99490  3.15       0.46      9.6        5
+4895            6.5              0.24         0.19             1.2      0.041                 30.0                 111.0  0.99254  2.99       0.46      9.4        6
+4896            5.5              0.29         0.30             1.1      0.022                 20.0                 110.0  0.98869  3.34       0.38     12.8        7
+4897            6.0              0.21         0.38             0.8      0.020                 22.0                  98.0  0.98941  3.26       0.32     11.8        6
+'''
+
+print(wine.shape)
+# (4898, 12)
+print(wine.describe())
+'''
+       fixed acidity  volatile acidity  citric acid  residual sugar    chlorides  free sulfur dioxide  total sulfur dioxide      density           pH    sulphates      alcohol      quality
+count    4898.000000       4898.000000  4898.000000     4898.000000  4898.000000          4898.000000           4898.000000  4898.000000  4898.000000  4898.000000  4898.000000  4898.000000
+mean        6.854788          0.278241     0.334192        6.391415     0.045772            35.308085            138.360657     0.994027     3.188267     0.489847    10.514267     5.877909
+std         0.843868          0.100795     0.121020        5.072058     0.021848            17.007137             42.498065     0.002991     0.151001     0.114126     1.230621     0.885639
+min         3.800000          0.080000     0.000000        0.600000     0.009000             2.000000              9.000000     0.987110     2.720000     0.220000     8.000000     3.000000
+25%         6.300000          0.210000     0.270000        1.700000     0.036000            23.000000            108.000000     0.991723     3.090000     0.410000     9.500000     5.000000
+50%         6.800000          0.260000     0.320000        5.200000     0.043000            34.000000            134.000000     0.993740     3.180000     0.470000    10.400000     6.000000
+75%         7.300000          0.320000     0.390000        9.900000     0.050000            46.000000            167.000000     0.996100     3.280000     0.550000    11.400000     6.000000
+max        14.200000          1.100000     1.660000       65.800000     0.346000           289.000000            440.000000     1.038980     3.820000     1.080000    14.200000     9.000000
+'''
+
+wine_npy = wine.to_numpy() # == wine.values
+
+x = wine_npy[:,:-1]
+y = wine_npy[:,-1]
+
+print(x.shape, y.shape)
+# (4898, 11) (4898,)
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler,RobustScaler
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
+)
+
+scale = StandardScaler()
+scale.fit(x_train)
+x_train = scale.transform(x_train)
+x_test = scale.transform(x_test)
+
+print(x_train.shape, x_test.shape)
+# (3918, 11) (980, 11)
