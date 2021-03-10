e9bc479572256d08a936c51dce3c2f99acd9b8be
study <sswwd95@gmail.com>
2021-03-10 18:30:31 +0900

    commit

--- tf114(tensorflow1)/tf11_2_diabetes.py
+++ tf114(tensorflow1)/tf11_2_diabetes.py
@@ -1,7 +1,51 @@
 from sklearn.datasets import load_diabetes # 회귀
 import tensorflow as tf
+from sklearn.metrics import r2_score
+
+# 실습 : 최종 sklearn의 accuracy_score값으로 결론낼 것
+
+dataset = load_diabetes()
+
+x_data = dataset.data
+y_data = dataset.target
+
+print(x_data.shape, y_data.shape)
+# (506, 13) (506,)
+y_data = y_data.reshape(-1,1)
+print(x_data.shape, y_data.shape)
+# (442, 10) (442, 1)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+    x_data, y_data, train_size = 0.8, random_state = 66)
+
 
 x = tf.placeholder(tf.float32,shape=[None,10])
 y = tf.placeholder(tf.float32, shape=[None,1])
 
-#실습! 만들기
+w = tf.Variable(tf.random_normal([10,1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = tf.matmul(x,w) + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y))
+
+train = tf.train.AdamOptimizer(learning_rate=0.9).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(5001):
+        _,cost_val, h_val = sess.run([train, cost, hypothesis],feed_dict={x:x_train, y:y_train})
+        
+        y_pred = sess.run(hypothesis, feed_dict={x:x_test})
+        
+        if step % 1000 ==0 :
+            print(step, cost_val, h_val)
+    
+    print("R2 : ", r2_score(y_test,y_pred))
+
+
+# R2 :  0.5052302650286769
+
+
