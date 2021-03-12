c6a0dae4f67f199f6055d9d3a82f8625f9a39bdb
study <sswwd95@gmail.com>
2021-03-12 10:55:27 +0900

    commit

--- keras2/keras86_BatchNomalization.py
+++ keras2/keras86_BatchNomalization.py
@@ -20,17 +20,26 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.layers import BatchNomalization, Activation
 from tensorflow.keras.regularizers import l1,l2,l1_12
+# regularizers :정칙화, 정규화? 
 
+# minmax 정규화, standard 일반화? 
 model = Sequential()
-model.add(Conv2D(filters=10, kernel_size=(2,2), padding='same', strides=1, input_shape=(28,28,1)))
-model.add(BatchNormalization())
+model.add(Conv2D(filters=64, kernel_size=(2,2), padding='same', strides=1, input_shape=(28,28,1)))
+model.add(BatchNomalization()) # 정규화, 일반화? 
 model.add(Activation('relu'))
 
-model.add(Conv2D(9,2))
-model.add(Conv2D(8,2))
+model.add(Conv2D(32,2, kernel_initializer='he_normal'))
+model.add(BatchNomalization())
+model.add(Activation('relu'))
+
+model.add(Conv2D(32,2, kernel_regularizer=l1(l1=0.01)))
 model.add(Dropout(0.2))
+
+model.add(Conv2D(32,2, strides=2))
+
+
 model.add(Flatten()) 
-model.add(Dense(40,activation='relu'))
+model.add(Dense(32,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
 
@@ -53,3 +62,44 @@ print(y_predict)
 print(y_test[:10])
 # loss, acc :  0.06896616518497467 0.9800999760627747
 
+# layer상에서 한정짓는 것들 정리
+'''
+kernel_initializer : He        relu친구들?
+(가중치 초기화)       Xavier    sigmoid, tanh?
+
+bias_initializer 
+kernel_regularizer
+
+batchNormalization
+Dropout
+
+
+batchNormalization과 Dropout은 통상적으로 같이 쓰면 안된다고 한다. 성능에 중복적인 부분이 있다
+근데 gan에서는 같이쓴다. 명확히 좋다는 것은 없음
+
+
+
+
+
+kernel_initializer : He - relu, selu, elu ....
+                     Xavier - sigmoid, tahn
+(kernel : 가중치(weight))
+                     kernel_initializer 를 하게 되면 얼마나 gradient 를 잘 전달 할 수 있느냐와
+                     layer 를 얼마나 깊게 쌓을 수 있느냐가 정해짐
+                     kernel_initializer 에 존재하는 he 와 xavier 는 각각
+                     relu, selu, elu 등과 sigmoid, tahn 등에 사용할 때 적합하다
+
+bias_initializer : bias 는 활성화 함수에 직접적으로 관여하게 되므로 몹시 중요한데,
+                   기존에는 0.01 이나 0.1 처럼 매우 작은 양수를 주었으나,
+                   학습 방법이 개선 된 지금은 보통 0 으로 초기화를 시킴
+
+kernel_regularizer : 레이어 복잡도에 제한을 두어 가중치가 가장 작은 값을 가지도록 강제함
+                     (가중치 값의 분포가 균일해짐)
+
+BatchNormalization : 레이어에 들어가는 batch 값들을 정규화 시킴
+
+Dropout : 훈련 할 때 node 의 갯수를 무작위로 줄임 / 검증할 때엔 dropout 을 하지 않음
+
+Batch, Dropout 과 같이 쓰면 안 좋다고는 하지만 무조건 확정적인 것은 아니며,
+실제로도 gan 에서도 함께 쓰이기도 한다
+'''
--- tf114(tensorflow1)/tf16_mnist4.py
+++ tf114(tensorflow1)/tf16_mnist4.py
@@ -19,39 +19,87 @@ x_test = x_test.reshape(10000,28*28).astype('float32')/255.
 x = tf.placeholder('float',[None,784])
 y = tf.placeholder('float',[None,10])
 
-w1 = tf.Variable(tf.random_normal([784,100]), name = 'weight1')
+# ------------------ 첫번째 레이어---------------------
+# w1 = tf.Variable(tf.random_normal([784,100]), name = 'weight1')
 # get_variable과 Variable의 차이점? 메모리 구성에 차이가 있긴 하지만 거의 비슷, 변수를 구한다
 w1 = tf.get_variable('weight1', shape=[784,100],
                     initializer=tf.contrib.layers.xavier_initializer()) 
+                    # xavier_initializer = kernel_initializer
+print('w1 : ', w1)
+# w1 :  <tf.Variable 'weight1:0' shape=(784, 100) dtype=float32_ref>
 
 b1 = tf.Variable(tf.random_normal([100]), name = 'bias1')
+print('b1:',b1)
+# b1: <tf.Variable 'bias1:0' shape=(100,) dtype=float32_ref>
 
 # layer1 = tf.nn.softmax(tf.matmul(x,w1) + b1) # 히든레이어에 softmax있으면 잘 안돌아간다
 # layer1 = tf.nn.elu(tf.matmul(x,w1) + b1) 
 # layer1 = tf.nn.selu(tf.matmul(x,w1) + b1) 
 layer1 = tf.nn.relu(tf.matmul(x,w1) + b1)
+print('layer1 : ', layer1)
+# layer1 :  Tensor("Elu:0", shape=(?, 100), dtype=float32)
+
 layer1 = tf.nn.dropout(layer1, keep_prob=0.3) 
 # keep_prob = 0.3 :30% drop out한다는 것
-"""
-w2 = tf.Variable(tf.random_normal([100,50]), name = 'weight2')
-b2 = tf.Variable(tf.random_normal([50]), name = 'bias2')
+
+print('layer1 : ', layer1)
+# layer1 :  Tensor("dropout/mul_1:0", shape=(?, 100), dtype=float32)
+# ----------------------------------------------------------------------
+
+w2 = tf.get_variable('weight2', shape=[100,128],
+                    initializer=tf.contrib.layers.xavier_initializer()) 
+b2 = tf.Variable(tf.random_normal([128]), name = 'bias2')
 layer2 = tf.nn.relu(tf.matmul(layer1,w2) + b2)
 layer2 = tf.nn.dropout(layer2, keep_prob=0.3) 
 
+w3 = tf.get_variable('weight3', shape=[128,64],
+                    initializer=tf.contrib.layers.xavier_initializer()) 
+b3 = tf.Variable(tf.random_normal([64]), name = 'bias3')
+layer3 = tf.nn.relu(tf.matmul(layer2,w3) + b3)
+layer3 = tf.nn.dropout(layer3, keep_prob=0.3) 
 
-w3 = tf.Variable(tf.random_normal([50,10]), name = 'weight3')
-b3 = tf.Variable(tf.random_normal([10]), name = 'bias3')
-hypothesis = tf.nn.relu(tf.matmul(layer2,w3) + b3)
+w4 = tf.get_variable('weight4', shape=[64,10],
+                    initializer=tf.contrib.layers.xavier_initializer()) 
+b4 = tf.Variable(tf.random_normal([10]), name = 'bias4')
+hypothesis = tf.nn.softmax(tf.matmul(layer3,w4) + b4)
 
+# 3. 컴파일, 훈련
 loss = tf.reduce_mean(-tf.reduce_mean(y*tf.log(hypothesis), axis=1))
 
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer()) # 변수 초기화
+# training_epochs = 15
+# batch_size = 100
+training_epochs = 100
+batch_size = 100
+total_batch = int(len(x_train)/batch_size)
+# 60000/100 = 600, 1에폭당 600번
 
-    for step in range(2001):
-        _, loss_val = sess.run([optimizer, loss], feed_dict={x:x_train, y:y_train})
-        if step % 200 ==0:
-            print(step, loss_val)
-"""
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(training_epochs):
+    avg_loss= 0
+
+    for i in range(total_batch):  # 600번 돈다
+        start = i * batch_size
+        end = start + batch_size
+
+        batch_x, batch_y = x_train[start:end], y_train[start:end]
+        # 1에폭에서 첫 번째 배치는 100개의 데이터. batch_x, y ->  0~100번째까지
+        feed_dict = {x:batch_x, y:batch_y}
+        l, _ = sess.run([loss,optimizer], feed_dict=feed_dict)
+        # 100개의 cost라서 나머지 59900개도 해야한다
+        avg_loss += l/total_batch
+        # avg_cost? cost구한거에서 600을 나누기때문에 전체 cost가 나온다
+        # 1에폭에 600번이 도는데 한 번 돌때마다 loss가 나온다 
+        # 600개의 loss를 다 더해서 다시 600으로 나누면 loss의 평균값이 나온다 = 6만개의 loss 평균값이라고 생각할 수 있다
+        # 데이터가 너무 크니까 한번에 6만개 돌면 터진다 그래서 나눠서 600개만 한다
+    print('epoch : ', '%04d' %(epoch+1),
+          'loss = {:.9f}'.format(avg_loss))
+print("끝")
+
+prediction = tf.equal(tf.math.argmax(hypothesis, 1),tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+print('acc :', sess.run(accuracy, feed_dict={x:x_test, y:y_test}))
