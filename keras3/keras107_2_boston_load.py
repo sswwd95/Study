8e5fe9a78c2dbe5b6a368d9be71bb7b60d637412
sswwd95 <sswwd95@gmail.com>
2021-05-04 17:30:01 +0900

    commit

--- keras3/keras107_2_boston_load.py
+++ keras3/keras107_2_boston_load.py
@@ -1,4 +1,3 @@
-# 오토케라스에 회귀가 먹힐까?
 import tensorflow as tf
 import numpy as np
 import autokeras as ak
@@ -14,71 +13,90 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, train_size = 0.8, random_state = 42
 )
 
-print(x_train.shape, x_test.shape)
-print(x.shape, y.shape)
-# (506, 13) (506,)
+from tensorflow.keras.models import load_model
+model = load_model('C:/data/h5/ak_boston.h5')
+model.summary()
+
+'''
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 28, 28, 1)]       0
+_________________________________________________________________
+cast_to_float32 (CastToFloat (None, 28, 28, 1)         0
+_________________________________________________________________
+normalization (Normalization (None, 28, 28, 1)         3
+_________________________________________________________________
+conv2d (Conv2D)              (None, 26, 26, 32)        320
+_________________________________________________________________
+conv2d_1 (Conv2D)            (None, 24, 24, 64)        18496
+_________________________________________________________________
+max_pooling2d (MaxPooling2D) (None, 12, 12, 64)        0
+_________________________________________________________________
+dropout (Dropout)            (None, 12, 12, 64)        0
+_________________________________________________________________
+flatten (Flatten)            (None, 9216)              0
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 9216)              0
+_________________________________________________________________
+dense (Dense)                (None, 10)                92170
+_________________________________________________________________
+classification_head_1 (Softm (None, 10)                0
+=================================================================
+Total params: 110,989
+Trainable params: 110,986
+Non-trainable params: 3
+_________________________________________________________________
+'''
 
 
-#2 . 모델구성
-model = ak.StructuredDataRegressor(
-    overwrite=True, # true, false 큰 효과 모르겠다
-    max_trials=2, # 최대 시도 횟수 설정
-    loss = 'mse',
-    metrics=['mae'],
-    directory='C:/data/ak/'
-)
+best_model = load_model('C:/data/ak/structured_data_regressor/best_model')
+best_model.summary()
+'''
+_________________________________________________________________
+Model: "model"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 28, 28, 1)]       0
+_________________________________________________________________
+cast_to_float32 (CastToFloat (None, 28, 28, 1)         0
+_________________________________________________________________
+normalization (Normalization (None, 28, 28, 1)         3
+_________________________________________________________________
+conv2d (Conv2D)              (None, 26, 26, 32)        320
+_________________________________________________________________
+conv2d_1 (Conv2D)            (None, 24, 24, 64)        18496
+_________________________________________________________________
+max_pooling2d (MaxPooling2D) (None, 12, 12, 64)        0
+_________________________________________________________________
+dropout (Dropout)            (None, 12, 12, 64)        0
+_________________________________________________________________
+flatten (Flatten)            (None, 9216)              0
+_________________________________________________________________
+dropout_1 (Dropout)          (None, 9216)              0
+_________________________________________________________________
+dense (Dense)                (None, 10)                92170
+_________________________________________________________________
+classification_head_1 (Softm (None, 10)                0
+=================================================================
+Total params: 110,989
+Trainable params: 110,986
+Non-trainable params: 3
+_________________________________________________________________
+'''
+###################################################################
 
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
-es = EarlyStopping(patience=4, verbose=1, restore_best_weights=True, monitor='val_loss', mode='min')
-lr = ReduceLROnPlateau(patience=2, factor=0.5, verbose=1)
-cp = ModelCheckpoint(monitor='val_loss', filepath='C:/data/mc/', save_best_only=True, save_weights_only=True)
+result = model.evaluate(x_test, y_test)
+print(result)
 
-model.fit(x_train, y_train, epochs=10)  # 1에폭 돌때 배치사이즈 8번 나눠서 해서 64번돈다
+best_result = best_model.evaluate(x_test, y_test)
+print(best_result)
 
-# 4. 평가 예측
-loss, mae = model.evaluate(x_test, y_test, batch_size=64)
-print("loss, mae : ", loss, mae)
+# 313/313 [==============================] - 2s 2ms/step - loss: 0.0611 - acc: 0.9795
+# [0.06110234558582306, 0.9794999957084656]
+# 313/313 [==============================] - 1s 1ms/step - loss: 0.0611 - acc: 0.9795
+# [0.06110234558582306, 0.9794999957084656]
 
-y_predict = model.predict(x_test)
-
-from sklearn.metrics import mean_squared_error
-def RMSE(y_test, y_predict) : 
-    return np.sqrt(mean_squared_error(y_test, y_predict))
-print("RMSE : ", RMSE(y_test, y_predict))
-
-from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
-print("R2 : ", r2)
-
-model_ak = model.export_model()
-try:
-    model.save("C:/data/h5/ak_boston", save_format="tf")
-except Exception:
-    model.save('C:/data/h5/ak_boston.h5')
-
-
-best_model = model.tuner.get_best_model()
-try:
-    model.save("C:/data/h5/best_ak_boston", save_format="tf")
-except Exception:
-    model.save('C:/data/h5/best_ak_boston.h5')
-
-# 저장 에러날 때
-# https://autokeras.com/tutorial/export/
-
-# model3 = load_model('ak_save_boston', custom_objects=ak.CUSTOM_OBJECTS)
-# result_boston = model3.evaluate(x_test, y_test)
-
-# y_pred = model3.predict(x_test)
-# r2 = r2_score(y_test, y_pred)
-
-# print("load_result :", result_boston, r2)
-
-
-# ImageRegressor
-# ValueError: Expect the data to ImageInput to have shape (batch_size, height, width, channels) or (batch_size, height, width) dimensions, but got input shape [64, 13]
-
-# StructuredDataRegressor
-# loss, mae :  15.099772453308105 2.5873918533325195
-# RMSE :  3.885370964657517
-# R2 :  0.7941452509002509
+# https://autokeras.com/image_classifier/ 오토케라스 홈페이지에 정리 잘 되어 있따!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# trial은 2 이상으로 주는게 좋다
