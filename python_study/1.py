4fc941e34a33b075291b19b01982e3a12fe4298c
sswwd95 <sswwd95@gmail.com>
2021-06-02 23:48:57 +0900

    commit

---
+++ python_study/1.py
@@ -0,0 +1,17 @@
+#화면에 Hello World 문자열을 출력하세요.
+print("Hello World")
+
+# 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+print("Mary's cosmetics")
+
+# 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
+print('신씨가 소리질렀다. "도둑이야"')
+
+# 화면에 "C:\Windows"를 출력하세요.
+print('"C:\Windows"')
+
+# 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
+print("안녕하세요.\n만나서\t\t반갑습니다")
+# \n : 줄바꿈
+# \t : 탭 기능
+
